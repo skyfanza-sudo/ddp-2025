bd16f39d80ca895b2638f89ed14751557ffc7c03
skyfanza-sudo <skyfanza@gmail.com>
2025-11-18 19:56:01 -0800

    Add files via upload

---
+++ latihan praktikum9/latihan1.py
@@ -0,0 +1,6 @@
+#celcius_to_fahrenheit
+def t(suhu_celcius):
+    t = (suhu_celcius * 9/5) + 32
+    return t
+print(t(0))
+print(t(100))
---
+++ latihan praktikum9/latihan3.py
@@ -0,0 +1,9 @@
+def nilai(i = 0):
+    if i <=60:
+        print("tidak lulus")
+    elif i >  60 and i <= 100:
+        print("lulus")
+    else:
+        print("tidak diketahui")
+
+nilai(20)
